30ca11be1fe5eaa875fba6c2d5920c9ba46a41c3
md al-amin <muhammadalamin809@gmail.com>
2023-02-27 02:04:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function noFunction(){
+   alert(`আমি তুমাকে অনেক ভালোবাসি 
+                        প্লিজ, হ্যা বলে দাও `);
+
+
+}
+function yesFunction(){
+    window.location.href = 'index2.html';   
+}
